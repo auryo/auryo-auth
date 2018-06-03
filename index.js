e5c8533e242cf747f3a1f347634e22ba015a7f7b
jonassnellinckx <jo.snel@hotmail.com>
2018-06-03 11:39:26 +0200

    keep secrets in backend even when on dev

--- index.js
+++ index.js
@@ -11,13 +11,39 @@ app.use(function (req, res, next) {
     next()
 })
 
-app.get(process.env.CALLBACK, function (req, res) {
+app.get('/connect', (req, res) => {
     const socket_id = req.query.state
-    const code = req.query.code
+    const env = req.query.env
 
+    if (!socket_id || !env) {
+        res.status(422)
+            .send('Params are required')
+        return
+    }
+
+    const redirect_url = (env === 'development') ? 'http://localhost:3716/oauth/callback' : 'http://api.auryo.com/callback'
+    const client_id = (env === 'development') ? process.env.CLIENT_ID_DEV : process.env.CLIENT_ID
+
+    return res.redirect(`https://soundcloud.com/connect?client_id=${client_id}&response_type=code&scope=non-expiring&state=${env}-${socket_id}&redirect_uri=${redirect_url}`)
+})
+
+app.get(process.env.CALLBACK, function (req, res) {
+    let socket_id = req.query.state
+    const code = req.query.code
+    let env
     if (req.query.error) {
         res.io.to(socket_id).emit('error', req.query.error_description)
         res.redirect('http://auryo.com')
+        return
+    }
+
+    if (socket_id && socket_id.split('-').length === 2) {
+        env = socket_id.split('-')[0]
+        socket_id = socket_id.split('-')[1]
+
+        if (env === 'development') {
+            return res.redirect(`http://api.auryo.com/callback${serialize(req.query)}`)
+        }
     }
 
     if (!socket_id || !code) {
@@ -26,14 +52,18 @@ app.get(process.env.CALLBACK, function (req, res) {
         return
     }
 
+    const client_id = (env && env === 'development') ? process.env.CLIENT_ID_DEV : process.env.CLIENT_ID
+    const client_secret = (env && env === 'development') ? process.env.CLIENT_SECRET_DEV : process.env.CLIENT_SECRET
+    const redirect_uri = (env && env === 'development') ? 'http://localhost:3716/oauth/callback' : 'http://api.auryo.com/callback'
+
     request.post(
         'https://api.soundcloud.com/oauth2/token',
         {
             json: true,
             form: {
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET,
-                redirect_uri: process.env.REDIRECT_URL,
+                client_id,
+                client_secret,
+                redirect_uri,
                 grant_type: 'authorization_code',
                 code: code
             }
@@ -49,5 +79,12 @@ app.get(process.env.CALLBACK, function (req, res) {
         }
     )
 })
+
+function serialize(obj) {
+    return '?' + Object.keys(obj).reduce(function (a, k) {
+        a.push(k + '=' + encodeURIComponent(obj[k]))
+        return a
+    }, []).join('&')
+}
 
 module.exports = app
